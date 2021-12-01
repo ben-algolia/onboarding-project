d1f2d6ecfa7a7640e84fbc6f7f1b43370d6fb609
Ben Chad <ben.chad@algolia.com>
2021-12-01 12:46:24 +1100

    Updated indexing process to remove limit on number of indexed records

--- index-data.php
+++ index-data.php
@@ -3,24 +3,29 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-define('MAX_RECORDS', 9950); // Set to a positive integer, or false for no limit
+define('MAX_RECORDS', false); // Set to a positive integer, or false for no limit
 
-$client = Algolia\AlgoliaSearch\SearchClient::create(
-    'I98ERVUJ34',
-    'b9250962d37704b5654a6c3df9dd7275'
-);
+define('INDEX_ID', 'I98ERVUJ34');
+define('API_KEY', 'b9250962d37704b5654a6c3df9dd7275');
+define('INDEX_NAME', 'concerts');
 
-$index = $client->initIndex('onboarding-project');
+define('DATA_FILENAME', 'Data/alternative_rock_artists.json');
+
+$client = Algolia\AlgoliaSearch\SearchClient::create(INDEX_ID, API_KEY);
+
+$index = $client->initIndex(INDEX_NAME);
 
 // Index records
 try {
-    $records = json_decode(file_get_contents('Data/alternative_rock_artists.json'), true);
+    $records = json_decode(file_get_contents(DATA_FILENAME), true);
     if (MAX_RECORDS !== false) {
         // Truncate $records to MAX_RECORDS entries
         array_splice($records, MAX_RECORDS);
     }
     echo 'Attempting to index ' . count($records) . " records \n";
-    $index->saveObjects($records, ['autoGenerateObjectIDIfNotExist' => true]);
+    $index->saveObjects($records, [
+        'autoGenerateObjectIDIfNotExist' => true,
+    ]);
     echo 'Records indexed!';
 } catch (Exception $e) {
     echo $e->getMessage();
