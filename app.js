f11212cd94c875e7f4b6f435d7b16e3195ea146b
Ben Chad <ben.chad@algolia.com>
2022-03-17 14:41:40 +1100

    Committing current state

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-// Replace with your own values
+const { autocomplete } = window['@algolia/autocomplete-js'];
+const { getAlgoliaResults } = window['@algolia/autocomplete-js'];
+
 const searchClient = algoliasearch(
   'I98ERVUJ34',
   'c43b258d6b10e132df6239366cb268eb' // search only API key, not admin API key
 );
 
 const search = instantsearch({
-  indexName: 'onboarding-project',
+  indexName: 'concerts',
   searchClient,
   routing: true,
 });
 
 search.addWidgets([
   instantsearch.widgets.configure({
-    hitsPerPage: 10,
+    //hitsPerPage: 5,
+    aroundLatLngViaIP: true,
   })
 ]);
 
 search.addWidgets([
   instantsearch.widgets.searchBox({
     container: '#search-box',
-    placeholder: 'Search for concerts',
+    placeholder: 'Type any artist or venue',
   })
 ]);
+
+search.addWidgets([
+  instantsearch.widgets.hitsPerPage({
+    container: '#hits-per-page',
+    items: [
+      { label: '5 hits per page', value: 5, default: true },
+      { label: '10 hits per page', value: 10 },
+    ],
+  })
+]);
+
+instantsearch.widgets.currentRefinements({
+  container: '#current-refinements',
+});
 
 search.addWidgets([
   instantsearch.widgets.hits({
